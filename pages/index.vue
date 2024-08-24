c557e6af1e927496987d078f8f9eec56e09754b0
high5 <high525jp@gmail.com>
2024-08-24 23:05:24 +0900

    index.vue 修正

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const a = 'test'
+console.log(a)
+</script>
+
+<template>
+  <div>
+    <div>
+      test
+    </div>
+  </div>
+</template>
+
+<style scoped lang="css">
+.Margin {
+  margin-top: 10px;
+}
+
+.SampleData {
+  background-color: #ffeeed;
+}
+
+.ToLogin {
+  background-color: #ffeeed;
+}
+</style>
