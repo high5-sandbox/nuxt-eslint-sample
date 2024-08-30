94563cf46030c4045038cb0cad515b9a076733d3
high5 <high525jp@gmail.com>
2024-08-30 17:44:44 +0900

    vitestとnuxt.configの設定を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  app: {
+    head: {
+      htmlAttrs: {
+        style: 'background-color: #111; color: #f0f0f0;',
+      },
+    },
+  },
+  modules: ['@nuxt/eslint', '@nuxt/test-utils/module'],
 })
